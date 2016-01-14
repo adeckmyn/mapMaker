a1719aed66e6d748bcd6ee7abe5f5338675a69e4
Alex Deckmyn <alex.deckmyn@gmail.com>
2016-01-14 22:04:39 +0100

    Update example script

--- inst/examples/build_world.R
+++ inst/examples/build_world.R
@@ -1,9 +1,11 @@
 library(mapMaker)
-infile <- "~/code/NaturalEarth/v3.1.0/data/ne_50m_admin_0_countries_lakes"
-outfile <- "~/code/NaturalEarth/build.world50/w50"
-outfile2 <- "~/code/NaturalEarth/build.world50/w50b"
+infile <- "~/code/NaturalEarth/v3.1.0/data/ne_50m_admin_0_countries"
+infile.lakes <- "~/code/NaturalEarth/v3.1.0/data/ne_50m_admin_0_countries_lakes"
+outfile <- "~/code/NaturalEarth/build.world50/w50nl"
+outfile2 <- "~/code/NaturalEarth/build.world50/w50nl2"
 Sys.setenv("MYMAPS"=paste0(dirname(outfile),"/"))
 assign(paste0(basename(outfile),"MapEnv"),"MYMAPS")
+assign(paste0(basename(outfile2),"MapEnv"),"MYMAPS")
 
 ######################
 
@@ -44,35 +46,31 @@ for(l in e1) assign(paste("e",l,sep=""),get.line(ww,l))
 ## for both Fiji and Russia: most logical to move west part by +360
 
 # Fiji: line/poly 537:557
-# (551=552)-(553=554)-(555 = 556)
-w552$x <- w552$x + 360
-w554$x <- w554$x + 360
-w556$x <- w556$x + 360
+# (539=540)-(541=542)-(543 = 544)
+w540$x <- w540$x + 360
+w542$x <- w542$x + 360
+w544$x <- w544$x + 360
 
 # plot(rbind(w552,e551))
 # points(w552,type="o",col=2)
-p551 <- rbind(e551[1:5,],w552[7:11,],e551[1,])
-# check:
-# plot(p551,type="o")
-# points(e551,type="o",col2)
-# points(w552,type="o",col=3)
-ww <- line.change(ww,551,p551)
+p539 <- rbind(e539[1:5,],w540[7:11,],e539[1,])
+ww <- line.change(ww,539,p539)
 
-p553 <- rbind(e553[1:4,],w554[4:7,],e553[1,])
-ww <- line.change(ww,553,p553)
+p541 <- rbind(e541[1:4,],w542[4:7,],e541[1,])
+ww <- line.change(ww,541,p541)
 
-p555 <- rbind(w556[2,],e555[2:56,],w556[c(5:7,2),])
-ww <- line.change(ww,555,p555)
+p543 <- rbind(w544[2,],e543[2:56,],w544[c(5:7,2),])
+ww <- line.change(ww,543,p543)
 
-# Russia: 1285=1328(main), 1296 = 1294
-w1285$x <- w1285$x + 360
-w1296$x <- w1296$x + 360
+# Russia: 1271=1314(main), 1282 = 1280
+w1271$x <- w1271$x + 360
+w1282$x <- w1282$x + 360
 
-p1294 <- rbind(e1294[1:11,],w1296[c(23:31,2:20),],e1294[14:17,]) 
-ww <- line.change(ww,1294,p1294)
+p1280 <- rbind(e1280[1:11,],w1282[c(23:31,2:20),],e1280[14:17,]) 
+ww <- line.change(ww,1280,p1280)
 
-p1328 <- rbind(e1328[1:633,],w1285[c(337:338,2:321),],e1328[636:4599,])
-ww <- line.change(ww,1328,p1328)
+p1314 <- rbind(e1314[1:633,],w1271[c(337:338,2:321),],e1314[636:4574,])
+ww <- line.change(ww,1314,p1314)
 
 # now remove superfluous polygons
 for (i in rev(sort(w1))) ww <- remove.line.gon(ww,i)
@@ -85,87 +83,47 @@ for (i in rev(sort(w1))) ww <- remove.line.gon(ww,i)
 # but for 'maps' I need to have lines that fit exactly.
 # I don't want 'no-one's land' because it screws up internal=FALSE plots.`
 
-# BE/NL border bug (Nl is now line 1074, not 1077 because 3 polygons were removed)
-p187 <- get.line(ww,187) #be
-p1074 <- get.line(ww,1074)
-p187[29:30,] <- p1074[120:119,]
-p1074 <- p1074[-121,]
-ww <- line.change(ww, 187, p187)
-ww <- line.change(ww, 1074, p1074)
+# BE/NL border bug
+g1=187 #BE
+g2=1061 #NL
+p1 <- get.line(ww,g1)
+p2 <- get.line(ww,g2)
+p1[29:30,] <- p2[119:118,]
+p2[120:118,] <- p1[29:31,]
+
+ww <- line.change(ww, g1, p1)
+ww <- line.change(ww, g2, p2)
 
 # AF/IR : Iran has 1 extra point in boundary (remove, or add to Afghanistan?)
-p859 <- get.line(ww, 859)
-p859 <- p859[-235,]
-ww <- line.change(ww, 859, p859)
+g1 <- 847 #IQ
+g2 <- 2 #AF
+p1 <- get.line(ww, g1)
+p2 <- get.line(ww, g2)
+p1 <- p1[-235,]
+ww <- line.change(ww, g1, p1)
 
 # China/India : many errors
 # maybe easier to correct by pasting the whole border from one to the other
-p442 <- get.line(ww,442)
-p851 <- get.line(ww,851)
-p851 <- rbind(p442[1694:1576,],p851[51:1287,])
-ww <- line.change(ww, 851, p851)
+g1 <- 430 # CH
+p1 <- get.line(ww,g1)
+g2 <- 839 # IN
+p2 <- get.line(ww,g2)
+p2 <- rbind(p1[1692:1574,],p2[51:1287,])
+ww <- line.change(ww, g2, p2)
 
-# Indonesia:89/Papua New G
-p792 <- get.line(ww,792)
-p1209 <- get.line(ww,1209)
-p1209 <- p1209[-(278:279),]
-ww <- line.change(ww, 1209, p1209)
+# Indonesia:89/Papua New G:
+g1 <- 780
+g2 <- 1195
+p1 <- get.line(ww,g1)
+p2 <- get.line(ww,g2)
+p2 <- p2[-(278:279),]
+ww <- line.change(ww,g2, p2)
 
 # create line database
 ww0 <- ww
 ww <- map.gon2line(ww0,precision=1E-8)
-ww1 <- ww
-
-######################
-### Just add water ###
-######################
-### add the "punched out" boundary lakes as polygons
-### so the will also be filled when fill=TRUE
-### we can define the border lakes from knowing the line numbers
-greatlakes <- c(-1922,-467)
-erie <- c(-1923,-465)
-ontario <- c(-1924,-463)
-spednic <- c(-1925,-375)
-## TODO: all islands in the great lakes still have border "0": adapt manually at the end
-## worldHires doesn't have tis feature, so I won't bother for now
-
-titicaca <- c(-301,-1490,-297,-1489)
-
-## there is something strange with lake Kariba: a little polygon of Zambia on the other shore...
-## a glitch when punching out the lakes?
-kariba <- c(-2056,-2058,-2050,-2057,-2053)
-malawi <- c(-1834,-1381,-1368,-1382)
-tanganyika <- c(-1836, -2055, -606, -229)
-kivu <- c(-604,-1723)
-edward <- c(-601,-1841)
-albert <- c(-599,-1842)
-
-victoria <- c(-1832,-1840, -1212 )
-
-constance <- c(-512,-659,-219)
-
-peipus <- c(-1689,-734)
-
-caspian <- c(-224,-1686,-1206,-1807,-1124)
-aral <- c(-1203,-1997,-1200,-1999)
-sarygamysh <- c(-1809,-1998)
-
-khanka <- c(-1685,-561)
-
-ww$names <- c(ww$names, "Great Lakes:Erie","Great Lakes:Superior,Huron,Michigan",
-              "Great Lakes:Ontario","Lake Spednic","Lake Titicaca",
-              "Lake Kariba","Lake Malawi","Lake Tanganyika","Lake Kivu",
-              "Lake Edward","Lake Albert","Lake Victoria",
-              "Lake Constance","Lake Peipus","Caspian Sea","Aral Sea","Lake Sarygamysh",
-              "Lake Khanka"
-)
-ww$gondata <- c(ww$gondata,NA,erie,NA,greatlakes,NA,ontario,NA,spednic,NA,titicaca,NA,
-                kariba,NA,malawi,NA,tanganyika,NA,kivu,NA,edward,NA,albert,NA,victoria,
-                NA,constance,NA,peipus,NA,caspian,NA,aral,NA,sarygamysh,NA,khanka)
-
-ww$gon <- gon.parse(ww)
 ww <- map.LR(ww)
-
+ww1 <- ww
 
 
 ### split meridian crossings (necessary for world2)
@@ -182,15 +140,28 @@ ww$line <- line.parse(ww)
 splits <- which(ww$x==0)
 for (i in rev(sort(splits))) ww <- line.split(ww, i)
 
+### A few additions:
+# NL: Ijsselmeer & Zuid-Flevoland
+wlakes <- map.make(read.worldmap(infile.lakes))
+ijssel <- get.line(wlakes,1078)
+flevo <- get.line(wlakes,1074)
+ww <- line.append(ww,ijssel)
+nl <- dim(ww$line)[1]
+ww <- gon.append(ww,nl,"Netherlands:IJsselmeer")
+ww <- line.append(ww,flevo)
+nl <- dim(ww$line)[1]
+ww <- gon.append(ww,nl,"Netherlands:Zuid-Flevoland")
 
+# Some very small 'enclaves' don't have an associated 'hole'
+# as a result, e.g. Vatican appears even with 'interior=FALSE'
+# Solution: have the 'Vatican' polgon twice (in reverse order)
+# but we don't cal this 'italy:hole' or anything, just 'Vatican'
+clist <- c("Vatican")
+for (cc in clist) {
+  ll <- get.gon(ww,which(ww$names==cc))
+  ww <- gon.append(ww,-ll,cc)
+}
 
-# CHECK
-#zl <- which(ww$line$length == 2)
-#zp <- unlist(lapply(zl,function(i) which.gon(ww,i)))
-
-# other check: look for polygons with more than one sea-border (0)
-# or plot without internal boundaries
-# or countries that have several common lines: probably there is a bug inbetween
 ww <- map.LR(ww)
 map.export.bin(ww, outfile)
 map.export.ascii(ww, outfile, ndec=8)
@@ -198,7 +169,7 @@ map.export.ascii(ww, outfile, ndec=8)
 ##############
 ### world2 ###
 ##############
-ww1 <- ww
+ww2 <- ww
 
 zzz=which(ww$x==0)
 zerolines=vapply(zzz,function(x) which.line(ww,x),1)
@@ -229,7 +200,7 @@ ww <- line.append(ww, bf.w.close)
 ww <- gon.append(ww, c(bf.w, dim(ww$line)[1]), name=paste0(ww$names[pgon],":west"))
 
 # Algeria
-pgon <- 498
+pgon <- 486
 al <- get.gon(ww,pgon)
 al.w <- al[c(10,1:4)]
 al.e <- al[5:9]
@@ -244,7 +215,7 @@ ww <- line.append(ww, al.w.close)
 ww <- gon.append(ww, c(al.w, dim(ww$line)[1]), name=paste0(ww$names[pgon],":west"))
 
 # Spain
-pgon <- 523
+pgon <- 511
 gon0 <- get.gon(ww,pgon)
 # plot(get.fullgon(ww,pgon),type='l')
 # lines(c(0,0),c(-60,60))
@@ -271,7 +242,7 @@ ww <- line.append(ww, gon.w2.close)
 ww <- gon.append(ww, c(gon.w2, dim(ww$line)[1]), name=paste0(ww$names[pgon],":west2"))
 
 # France
-pgon <- 570
+pgon <- 558
 gon0 <- get.gon(ww,pgon)
 # plot(get.fullgon(ww,pgon),type='l')
 # lines(c(0,0),c(-60,60))
@@ -291,7 +262,7 @@ nl <- dim(ww$line)[1]
 ww <- gon.append(ww, c(gon.w1, nl), name=paste0(ww$names[pgon],":west"))
 
 ## UK
-pgon <- 596
+pgon <- 584
 gon0 <- get.gon(ww,pgon)
 # plot(get.fullgon(ww,pgon),type='l')
 # lines(c(0,0),c(-60,60))
@@ -319,7 +290,7 @@ nl <- dim(ww$line)[1]
 ww <- gon.append(ww, c(gon.w2, nl), name=paste0(ww$names[pgon],":west2"))
 
 ## Ghana
-pgon <- 607
+pgon <- 595
 gon0 <- get.gon(ww,pgon)
 # plot(get.fullgon(ww,pgon),type='l')
 # lines(c(0,0),c(-60,60))
@@ -347,7 +318,7 @@ nl <- dim(ww$line)[1]
 ww <- gon.append(ww, c(gon.w2, nl), name=paste0(ww$names[pgon],":west2"))
 
 ## Togo
-pgon <- 1417
+pgon <- 1403
 gon0 <- get.gon(ww,pgon)
 # plot(get.fullgon(ww,pgon),type='l')
 # lines(c(0,0),c(-60,60))
@@ -375,7 +346,7 @@ nl <- dim(ww$line)[1]
 ww <- gon.append(ww, c(gon.e2, nl), name=paste0(ww$names[pgon],":east2"))
 
 ## Mali
-pgon <- 1005
+pgon <- 992
 gon0 <- get.gon(ww,pgon)
 # plot(get.fullgon(ww,pgon),type='l')
 # lines(c(0,0),c(-60,60))
@@ -393,18 +364,18 @@ ww <- line.append(ww, gon.w1.close)
 nl <- dim(ww$line)[1]
 ww <- gon.append(ww, c(gon.w1, nl), name=paste0(ww$names[pgon],":west"))
 
-
+g1=150
+g2=151
 ant.closure <- data.frame(x = c(360,seq(360,0,length=10),0),
-                          y=c(tail(get.line(ww,152)$y,1),rep(-88,10),get.line(ww,153)$y[1]))
+                          y=c(tail(get.line(ww,g1)$y,1),rep(-89.99,10),get.line(ww,g2)$y[1]))
 ww <- line.append(ww, ant.closure)
 nl <- dim(ww$line)[1]
 ww <- gon.change(ww,121, c(get.gon(ww,121),nl))
-ww <- change.gon(ww,121, c(get.gon(ww,121),nl))
+
 # a last little cleaning (error is 1.E-13, so it should dissappear anyway in output)
-ww$x[ww$line$end[153]] <- 180
+ww$x[ww$line$end[g2]] <- 180
 
 ww <- map.LR(ww)
 map.export.bin(ww, outfile2)
 map.export.ascii(ww, outfile2, ndec=8)
-
 
